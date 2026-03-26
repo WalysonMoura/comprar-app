184ab7f00d5cc4aeeb1ffcfa9a9161fdde20fc56
Walyson Moura <contato.walysondev@gmail.com>
2026-03-25 23:33:59 -0300

    chore: standardize quote styles in configuration files

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,10 +1,10 @@
-import type { UserConfig } from "@commitlint/types";
+import type { UserConfig } from '@commitlint/types'
 
 const Configuration: UserConfig = {
-  extends: ["@commitlint/config-conventional"],
+  extends: ['@commitlint/config-conventional'],
   rules: {
-    "body-max-line-length": [2, "always", 1120],
+    'body-max-line-length': [2, 'always', 1120],
   },
-};
+}
 
-export default Configuration;
+export default Configuration
