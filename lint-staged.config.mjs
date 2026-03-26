d3b1e154cb360daab17536127725b3016bda5077
Walyson Moura <contato.walysondev@gmail.com>
2026-03-25 23:29:44 -0300

    chore: add Prettier configuration and update import quotes for consistency

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import("lint-staged").Configuration} */
 const config = {
-  "*.{ts,tsx,js,jsx}": ["eslint --fix", "eslint", "prettier --write"],
-  "**/*.ts?(x)": () => "npm run type-check",
-  "**/*.test.ts": ["jest"],
-  "*.{json,yaml}": ["prettier --write"],
-};
+  '*.{ts,tsx,js,jsx}': ['eslint --fix', 'eslint', 'prettier --write'],
+  '**/*.ts?(x)': () => 'npm run type-check',
+  '**/*.test.ts': ['jest'],
+  '*.{json,yaml}': ['prettier --write'],
+}
 
-export default config;
+export default config
